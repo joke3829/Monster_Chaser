bee54a7075c5b5d1fe999569f3aed9b8a1c96966
Tujungmin <paulkjm92@gmail.com>
2025-05-08 16:28:29 +0900

    클라 좌표바꾸기

--- Monster_Chaser/Client/Client.cpp
+++ Monster_Chaser/Client/Client.cpp
@@ -86,7 +86,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 	// TODO: 여기에 코드를 입력합니다.
-	if (!Client.Init("172.0.0.1", PORT_NUM))            //Change IP Address
+	if (!Client.Init("172.30.1.18", PORT_NUM))            //Change IP Address
 	{
 		MessageBoxA(nullptr, "서버 연결 실패. 클라이언트를 종료합니다.", "연결 실패", MB_ICONERROR);
 		return 0;  // 창 생성 없이 종료
