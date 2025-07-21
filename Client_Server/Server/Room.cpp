9f6be9defa417d460df0bac2eff7f4644f221dd3
Tujungmin <paulkjm92@gmail.com>
2025-07-21 16:45:01 +0900

    2025-07-21

--- Client_Server/Server/Room.cpp
+++ Client_Server/Server/Room.cpp
@@ -109,12 +109,23 @@ void Room::SpawnMonsters()
 	if (!monsters.empty()) return;
 
 	// 예: 1스테이지 기준
-	if (stage == 1) {
+	switch (stage)
+	{
+	case 1:
 		monsters[new_id++] = std::make_shared<Monster>(1000, XMFLOAT3(-28.0f, 0.0f, -235.0f), MonsterType::Feroptere);
 		monsters[new_id++] = std::make_shared<Monster>(1001, XMFLOAT3(-28.0f, 0.0f, -245.0f), MonsterType::Pistiripere);
 		monsters[new_id++] = std::make_shared<Monster>(1002, XMFLOAT3(-28.0f, 0.0f, -255.0f), MonsterType::RostrokarackLarvae);
 		monsters[new_id++] = std::make_shared<Monster>(1003, XMFLOAT3(-28.0f, 0.0f, -265.0f), MonsterType::XenokarceBoss); // 보스
+		break;
+	case 2:
+		break; // 2스테이지 몬스터는 아직 정의되지 않음
+	case 3:
+		break; // 3스테이지 몬스터는 아직 정의되지 않음
+	default:
+		break;
 	}
+	
+
 }
 
 
